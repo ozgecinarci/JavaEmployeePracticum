c66011752d2cbc2d5e68d28754d778d58b837d64
ozgeozgec <89706915+ozgecinarci@users.noreply.github.com>
2022-10-16 22:07:47 +0300

    JavaEmployee

---
+++ src/src/Employee.java
@@ -0,0 +1,118 @@
+
+public class Employee {
+	private String name;
+	private float salary ;
+	private float  workHours ;
+	private int hireYear;
+	
+	public Employee (String name,float salary,int workHours, int hireYear){
+		this.name=name;
+		this.salary=salary;
+		this.workHours=workHours;
+		this.hireYear=hireYear;
+	}
+	
+	public void showInformations(){
+		System.out.println("Bilgiler...");
+		System.out.println("Ýsim: "+this.name);
+		System.out.println("Çalýþma Saati: "+this.workHours);
+		System.out.println("Ýþe baþladýðý yýl: "+this.hireYear);
+	
+	}
+	
+	public void tax(){
+		if(salary>1000){
+			double tax= 0.03;
+			System.out.println("Tax: "+ tax);
+		}
+		else if (salary<1000){
+		 int tax=0;
+		 System.out.println("Tax: "+tax);
+		}
+	}
+	
+	public void bonus(){
+		if(workHours>40){
+			float bonus= (workHours-40)*30;
+			System.out.println("Bonus: "+ bonus);
+		}
+	}
+	public void raiseSalary(){
+		int nowYear=2021;
+		if(nowYear-hireYear<10){
+			if(salary>1000){
+				if(workHours>40){
+				salary=(((salary*97)/100)*105)/100 + (workHours-40)*30;
+				System.out.println("Maaþ+Bonus: "+salary);
+				}
+				else{
+					salary=(((salary*97)/100)*105)/100;
+					System.out.println("Maaþ: "+salary);
+				}
+			}
+			if(salary<1000){
+				if (workHours>40){
+					salary=(salary*105)/100 + (workHours-40)*30;
+					System.out.println("Maaþ+Bonus: "+salary);
+				}
+				else{
+					salary=(salary*105)/100;
+					System.out.println("Maaþ: "+salary);
+				}
+			}
+		}
+		
+		
+		
+		
+		else if((nowYear-hireYear)>9 && (nowYear-hireYear)<20 ){
+			if(salary>1000){
+				if(workHours>40){
+				salary=(((salary*97)/100)*110)/100 + (workHours-40)*30;
+				System.out.println("Maaþ: "+salary);
+				}
+				else{
+					salary=(((salary*97)/100)*110)/100;
+					System.out.println("Maaþ: "+salary);
+				}
+			}
+			if(salary<1000){
+				if (workHours>40){ 
+					salary=(salary*110)/100 + (workHours-40)*30;
+					System.out.println("Maaþ: "+salary);
+				}
+				else{
+					salary=(salary*110)/100;
+					System.out.println("Maaþ: "+salary);
+				}
+			}
+			
+				
+		}
+		else if((nowYear-hireYear)>19){
+			if(salary>1000){
+				if(workHours>40){
+				salary=(((salary*97)/100)*115)/100 + (workHours-40)*30;
+				System.out.println("Maaþ: "+salary);
+				}
+				else{
+					salary=(((salary*97)/100)*115)/100;
+					System.out.println("Maaþ:" +salary);
+				}
+			}
+			if(salary<1000){
+				if (workHours>40){
+					salary=(salary*115)/100 + (workHours-40)*30;
+					System.out.println("Maaþ: "+salary);
+				}
+				else{
+					salary=(salary*115)/100;
+					System.out.println("Maaþ: "+salary);
+				}
+			}
+			
+		}
+		
+	}
+	
+}
---
+++ src/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main (String[] args){
+    Employee employee=new Employee("XX",15000,50,2021);
+	employee.showInformations();
+	employee.tax();
+	employee.bonus();
+	employee.raiseSalary();
+	
+
+}
+}
